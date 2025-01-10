dae536473a49f3545b3a33e9a6c0af00974176ff
N-i-ke <82163707+N-i-ke@users.noreply.github.com>
2025-01-10 11:14:57 +0900

    feat: video

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import videoFile from '../video/video.mp4';
 
 const Main: React.FC = () => {
   return (
     <section className="main">
       <div className="main_inner">
         <div className="main_video">
-          <video src="video/video.mp4" muted autoPlay loop playsInline></video>
+            <video src={videoFile} autoPlay muted loop />
         </div>
       </div>
     </section>
